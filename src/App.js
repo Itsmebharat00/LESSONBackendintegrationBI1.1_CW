e77f5ab215d4d2fc07b5da44b06c25ed10df1b53
Bharat Singh <74733419+Itsmebharat00@users.noreply.github.com>
2025-09-24 17:40:41 +0000

    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./styles.css";
+import Movies from "./components/Movies.jsx";
+import MovieByTitle from "./components/MoviesByTitle.jsx";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Movies />
+      <MovieByTitle title="Gully Boy" />
     </div>
   );
 }
